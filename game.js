7c88d5feaad1c66b7f1d7ebd3bf8b718f8a0b52a
Jarosław Bagnicki <jarek@bagnicki.net>
2018-07-09 07:00:57 +0200

    Logic: Add game object with basic methods

--- game.js
+++ game.js
@@ -1,8 +1,49 @@
-// Vars
+'use strict'
+
+const game = (function () {
+  let gameScore = {};
+
+  // PRIV scoreRefresh method 
+  function scoreRefresh () {
+    UIGameScore.innerHTML = `${gameScore.player} : ${gameScore.computer}`
+  };
+
+  // PRIV addLogMessage method 
+  function addLogMessage (message) {
+    UIGameLog.insertAdjacentHTML('beforeend',`[${new Date().toLocaleTimeString()}] ${message}`);
+    scoreRefresh();
+  }
+  
+  // PUBLIC newGame method
+  function newGame () {
+    gameScore.player = gameScore.computer = 0;
+    scoreRefresh();
+    addLogMessage('Game started.');
+  };
+
+  // PUBLIC playerMove method
+  function playerMove () {
+    console.log(this.id);
+  };
+  
+  return {
+    newGame,
+    playerMove
+  }
+})()
+
+// UI Vars
 const UIGameBtns = document.querySelectorAll('.game-btn');
 const UIGameScore = document.querySelector('#game-score');
 const UIGameLog = document.querySelector('#gamelog-board');
 
-UIGameBtns.forEach(btn => btn.addEventListener('click', function() {
-  console.log(this.id);
-}));
+const UIicons = {
+  rock: '<i class="fa fa-hand-rock"></i>',
+  paper: '<i class="fa fa-hand-paper"></i>',
+  scissors: '<i class="fa fa-hand-scissors"></i>'
+}
+
+UIGameBtns.forEach(btn => btn.addEventListener('click', game.playerMove));
+
+game.newGame();
+
