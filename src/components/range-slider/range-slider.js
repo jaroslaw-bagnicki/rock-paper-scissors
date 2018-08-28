d1446ad57bbeb78e58ac338eab4a11e8e3597964
Jarosław Bagnicki <jarek@bagnicki.net>
2018-08-28 09:00:46 +0200

    Cleanup. Remove unnecessary files

--- src/components/range-slider/range-slider.js
+++
@@ -1,8 +0,0 @@
-const UIMyRangeSlider = document.querySelector('#my-range');
-const UIMyRangeSliderLabel = document.querySelector('#my-range-label');
-
-// Binding 
-UIMyRangeSliderLabel.innerText = UIMyRangeSlider.value;
-UIMyRangeSlider.oninput = function () {
-  UIMyRangeSliderLabel.innerText = this.value;
-}
